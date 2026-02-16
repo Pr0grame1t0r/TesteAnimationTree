0f8f4c3a4ef3aa011cc9503529a6abe0a6189f21
Pr0grame1t0r <kavulack.arthur@gmail.com>
2026-02-16 03:27:32 -0300

    Configurei Animações e Shaders

---
+++ Assets/Paper/Player Controler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+
+    public float jumpForce;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
