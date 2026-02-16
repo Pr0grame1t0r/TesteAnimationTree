080eca6d72985dedb3d3eb731ec0d1d4d4007fc7
Pr0grame1t0r <kavulack.arthur@gmail.com>
2026-02-16 17:33:29 -0300

    Configurei o Normal,  apliquei parte dele, programei a movimentação base

--- Assets/Paper/Player Controler.cs
+++ Assets/Paper/Player Controler.cs
@@ -7,14 +7,41 @@ public class PlayerControler : MonoBehaviour
     public float speed;
 
     public float jumpForce;
+
+    public bool isGrounded;
+
+    public float Direction;
+
+    Rigidbody2D rb;
+
+    Animator anim;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            Direction = -1;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            Direction = 1;
+        }
+        anim.SetFloat("Direção", Direction);
+    }
+
+    void Movement()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Magnitude", movement.magnitude);
     }
 }
